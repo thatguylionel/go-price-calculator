ea01afd75d120c84ffd86b14debdc4a5578bbc7f
cawood <cawood@intime.de>
2024-02-25 13:26:41 +0100

    filemanager added

--- prices/prices.go
+++ prices/prices.go
@@ -1,10 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+
+	"github.com/thatguylionel/go-price-calculator/conversion"
+	"github.com/thatguylionel/go-price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -21,45 +21,20 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 }
 
 func (job *TaxIncludedPriceJob) loadData() {
-	file, err := os.Open("prices.txt")
-
+	lines, err := filemanager.ReadLines("prices.txt")
 	if err != nil {
-		fmt.Println("File could not open. Error occurred")
 		fmt.Println(err)
-		file.Close()
 		return
 	}
 
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
+	prices, err := conversion.StringsToFloats(lines)
 	if err != nil {
-		fmt.Println("Scanner error occurred")
+		fmt.Println("Convert price to float failed")
 		fmt.Println(err)
-		file.Close()
 		return
-	}
-
-	prices := make([]float64, len(lines))
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-		if err != nil {
-			fmt.Println("Convert price to float failed")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
 	}
 
 	job.InputPrices = prices
-
-	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
